83b4a2486317f7a3b0a04a6da77eaeaef8f27dee
Lorenzo Rivosecchi <lorenzo.rivosecchi@lacolonia.studio>
2022-04-29 12:27:56 +0200

    remove codesplitting because it doesn't work with shopify

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,11 +1,12 @@
 import { shared } from "./shared";
+import { init as initIndex } from "./templates/index";
 
 async function main() {
   // Run logic required by all pages
   shared();
-  // Run logic required by specific pages
+  // Run logic required by specific templates
   if (document.body.dataset.templateName === "index") {
-    (await import("./templates/index")).init();
+    initIndex();
   }
 }
 
