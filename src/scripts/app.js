a1477fa02a55555d06efaf5c035471b00d5d2de5
Lorenzo Rivosecchi <lorenzo.rivosecchi@lacolonia.studio>
2022-04-29 14:23:02 +0200

    get document sections and loop over them

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -4,8 +4,11 @@ import { init as initIndex } from "./templates/index";
 async function main() {
   // Run logic required by all pages
   shared();
+
+  const { templateName /*templateSuffix*/ } = document.body.dataset;
+
   // Run logic required by specific templates
-  if (document.body.dataset.templateName === "index") {
+  if (templateName === "index") {
     initIndex();
   }
 }
--- src/scripts/templates/index.js
+++ src/scripts/templates/index.js
@@ -1,3 +1,15 @@
 export function init() {
   console.log("homepage");
+  // Find all sections in the page
+  const sections = document.querySelectorAll("[data-section-name]");
+
+  for (const section of sections) {
+    console.log("section found:", section);
+
+    const { sectionName /* sectionId */ } = section.dataset;
+
+    if (sectionName === "my-section") {
+      // do something
+    }
+  }
 }
