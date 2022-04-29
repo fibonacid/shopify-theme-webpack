1bd0154f34f9de65fb8e85393b8e5ac67542a241
Lorenzo Rivosecchi <lorenzo.rivosecchi@lacolonia.studio>
2022-04-29 12:15:40 +0200

    create mechanism for running scripts that affect a specific template

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,8 +1,12 @@
 import { shared } from "./shared";
 
-function main() {
+async function main() {
   // Run logic required by all pages
   shared();
+  // Run logic required by specific pages
+  if (document.body.dataset.templateName === "index") {
+    (await import("./templates/index")).init();
+  }
 }
 
 document.addEventListener("DOMContentLoaded", main);
